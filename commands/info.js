69e28d9f3fbbc3b52c9993d023e47dc02c042511
ilan <ilan.shrir@gmail.com>
2020-12-28 23:14:21 +0200

    initial commit

---
+++ commands/info.js
@@ -0,0 +1,9 @@
+module.exports = 
+{
+    name: 'info',
+    description: "this command gives info.",
+    execute(message, args)
+    {
+        message.channel.send("Hello\nI was made by <@381379655665713155>\nI am a very cool bot\n😇");
+    }
+}
---
+++ main.js
@@ -0,0 +1,46 @@
+
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+ 
+const prefix = ';';
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles)
+{
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once("ready", () =>
+{
+    console.log(`Logged in as ${client.user.tag}!\n`);
+    
+    client.user.setStatus("Online");
+    client.user.setActivity("a Flying Rocket", { type: 'WATCHING' });
+});
+
+client.on("message", message =>
+{
+    if(!message.content.startsWith(prefix) || message.author.bot) { return; }
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === "hi")
+    {
+        client.commands.get('hi').execute(message, args);
+    }
+    else if(command === "info")
+    {
+        client.commands.get('info').execute(message, args);
+    }
+});
+
+client.login(process.env.TOKEN);
