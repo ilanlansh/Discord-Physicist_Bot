fa2d053c116445f08d097d0966c1e5441e996bac
ilan <ilan.shrir@gmail.com>
2020-12-30 07:18:05 +0200

    first work on ;kill

--- commands/kill.js
+++ commands/kill.js
@@ -4,6 +4,11 @@ module.exports =
     description: "this command kills 🙂.",
     execute(message, args)
     {
+        killed = args[0];
+        id = killed.replace("<!", "").replace(">", "");
+
+        console.log(id);
+
         message.channel.send("command under development...");
     }
 }
