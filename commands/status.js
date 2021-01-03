fd18da07a16b9941503e5d76d0f75c4bec9adb29
ilan <ilan.shrir@gmail.com>
2021-01-03 15:31:00 +0200

    ramoved status command console log

--- commands/status.js
+++ commands/status.js
@@ -33,39 +33,25 @@ module.exports =
                 return;
             }
 
-            let today = new Date();
-
-            let h = today.getHours();
-            let m = today.getMinutes();
-            let s = today.getSeconds();
-
-            if(h < 10) { h = '0' + h; }
-            if(m < 10) { m = '0' + m; }
-            if(s < 10) { s = '0' + s; }
-
             if(type == "play")
             {
                 client.user.setActivity(status, { type: 'PLAYING' });
                 message.channel.send(`status changed to **Playing ${status}**`);
-                console.log('[' + h + ':' + m + ':' + s + "] status changed to " + `Playing ${status}`);
             }
             else if(type == "watch")
             {
                 client.user.setActivity(status, { type: 'WATCHING' });
                 message.channel.send(`status changed to **Watching ${status}**`);
-                console.log('[' + h + ':' + m + ':' + s + "] status changed to " + `Watching ${status}`);
             }
             else if(type == "listen")
             {
                 client.user.setActivity(status, { type: 'LISTENING' });
                 message.channel.send(`status changed to **Listening to ${status}**`);
-                console.log('[' + h + ':' + m + ':' + s + "] status changed to " + `Listening to ${status}`);
             }
             else if(type == "compete")
             {
                 client.user.setActivity(status, { type: 'COMPETING' });
                 message.channel.send(`status changed to **Competing in ${status}**`);
-                console.log('[' + h + ':' + m + ':' + s + "] status changed to " + `Competing in ${status}`);
             }
             else if(type == "stream")
             {
@@ -74,7 +60,6 @@ module.exports =
                     url: 'https://www.twitch.tv/ilanlansh'
                 });
                 message.channel.send(`status changed to **Streaming ${status}**`);
-                console.log('[' + h + ':' + m + ':' + s + "] status changed to " + `Streaming ${status}`);
             }
         }
         else
