44426c7abfb70a21274e0cf050c7fd30289a2a96
ilan <ilan.shrir@gmail.com>
2021-01-03 12:26:07 +0200

    increased amount of possible gifs in ;kill command

--- commands/kill.js
+++ commands/kill.js
@@ -6,7 +6,7 @@ module.exports =
     description: "this command kills. 🙂",
     async execute(message, args, Discord, client)
     {
-        let url = `https://api.tenor.com/v1/search?q=kill&key=${process.env.TENORKEY}&limit=25`;
+        let url = `https://api.tenor.com/v1/search?q=kill&key=${process.env.TENORKEY}&limit=50`;
         let response = await fetch(url);
         let json = await response.json();
 
