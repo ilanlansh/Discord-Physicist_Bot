c009e347879d1014eb68287f09b9606c485f0f1d
ilan <ilan.shrir@gmail.com>
2021-02-09 21:55:11 +0200

    added more methods to format.js

--- util/format.js
+++ util/format.js
@@ -1,12 +1,82 @@
 module.exports =
 {
     /**
-     * Returns the discord codeblock versrion of a string.
+     * Returns the discord codeblock versrion of a string. (not to be confused with code)
      * @param {string} message A string that is meant to be sent as a message.
      * @return {string} A codeblock version of that messgae string.
      */
-    codeblock: (message) =>
+    codeblock(message)
     {
         return `\`\`\`${message}\`\`\``;
-    }
+    },
+
+    /**
+     * Returns the discord bold versrion of a string.
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} A bold version of that messgae string.
+     */
+    bold(message)
+    {
+        return `**${message}**`;
+    },
+
+    /**
+     * Returns the discord italic versrion of a string.
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} An italic version of that messgae string.
+     */
+    itali(message)
+    {
+        return `*${message}*`;
+    },
+
+    /**
+     * Returns the discord bold & italic versrion of a string.
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} A bold & italic version of that messgae string.
+     */
+    bolditalic(message)
+    {
+        return `***${message}***`;
+    },
+    
+    /**
+     * Returns the discord underlined versrion of a string.
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} An underlined version of that messgae string.
+     */
+    underline(message)
+    {
+        return `__${message}__`;
+    },
+
+    /**
+     * Returns the discord strikethrough versrion of a string.
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} A strikethrough version of that messgae string.
+     */
+    strikethrough(message)
+    {
+        return `~~${message}~~`;
+    },
+
+    /**
+     * Returns the discord spolier versrion of a string.
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} A spoiler version of that messgae string.
+     */
+    spoiler(message)
+    {
+        return `||${message}||`;
+    },
+
+    /**
+     * Returns the discord code versrion of a string. (not to be confused with codeblock)
+     * @param {string} message A string that is meant to be sent as a message.
+     * @return {string} A code version of that messgae string.
+     */
+    code(message)
+    {
+        return `\`${message}\``;
+    },
 }
