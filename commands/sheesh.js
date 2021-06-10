bd750481ae1eef56dc42f39ae0f95d3b94c001cb
ilan <ilan.shrir@gmail.com>
2021-06-11 00:02:35 +0300

    added a video to ;sheesh

--- commands/sheesh.js
+++ commands/sheesh.js
@@ -4,7 +4,8 @@ module.exports =
     description: "SHEEEEEEEEEEEEESH",
     execute(message, args)
     {
-        let url = "./resources/videos/sheesh.mp4";
+        let urls = ["./resources/videos/sheesh_sinatraa.mp4", "./resources/videos/sheesh_kyedae.mp4"];
+        let url = urls[Math.floor(Math.random()*urls.length)];
         message.channel.send({files: [url]});
     }
 };
